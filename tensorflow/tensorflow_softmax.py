e567760ce04be95fef9fdda481714d650c1f7e5c
OmerKirnap <okirnap@ku.edu.tr>
2016-09-29 09:35:16 +0300

    remove feed dict and faster implementation

--- tensorflow/tensorflow_softmax.py
+++ tensorflow/tensorflow_softmax.py
@@ -26,6 +26,7 @@ import timeit
 from tensorflow.examples.tutorials.mnist import input_data
 
 import tensorflow as tf
+import numpy as np
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -33,31 +34,51 @@ flags.DEFINE_string('data_dir', '/tmp/data/', 'Directory for storing data')
 
 mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
-sess = tf.InteractiveSession()
+batch_xs, batch_ys = mnist.train.next_batch(100)
+
 
 # Create the model
-x = tf.placeholder(tf.float32, [None, 784])
+x = tf.constant(batch_xs, name="x")
 W = tf.Variable(0.1*tf.random_normal([784, 10]))
 b = tf.Variable(tf.zeros([10]))
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+logits = tf.matmul(x, W) + b
 
-# Define loss and optimizer
-y_ = tf.placeholder(tf.float32, [None, 10])
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+# Define loss and optimizer, take only one single batch make forward, backward and update on it
+batch_y = batch_ys.astype(np.float32)
+y_ = tf.constant(batch_y, name="y_")
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, y_))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+  
+  
 
-# Train
-tf.initialize_all_variables().run()
-start_time = timeit.default_timer()
-while mnist.train.epochs_completed != 10:
-  batch_xs, batch_ys = mnist.train.next_batch(100)
-  train_step.run({x: batch_xs, y_: batch_ys})
-end_time = timeit.default_timer()
+# Train with single minibatch
+init = tf.initialize_all_variables()
+
+with tf.Session() as sess:
+  sess.run(init)
+  initial_loss = sess.run(loss)
+  print('Initial loss is ', initial_loss)
+  start_time = timeit.default_timer()
+  for i in range(5500):
+    sess.run(train_step)
+  end_time = timeit.default_timer()
+  final_loss = sess.run(loss)
+  print("Final loss is", final_loss)
 
 print("Total time is %s" % (end_time - start_time))
 
 # Test trained model This part agrees with the official documentation so the implementation is correct
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels}))
+# correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels}))
+
+# Naive implementation that is up to 2x slower
+# Train
+#tf.initialize_all_variables().run()
+#start_time = timeit.default_timer()
+#while mnist.train.epochs_completed != 10:
+#  batch_xs, batch_ys = mnist.train.next_batch(100)
+#  train_step.run({x: batch_xs, y_: batch_ys})
+#end_time = timeit.default_timer()
+
 
