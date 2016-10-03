aa4e0c0a9051549be06470e109b16345b16400d1
Ilker Kesen <ilkerksn@gmail.com>
2016-10-03 19:41:11 +0000

    first working version of housing

--- caffe/housing.py
+++ caffe/housing.py
@@ -10,24 +10,23 @@ import gc
 
 
 file_path = "/home/ec2-user/Knet8-Benchmarks/data/housing.data"
-
 data = np.loadtxt(file_path, dtype=np.float32)
 x = data[:, 0:13]
+x = (x - x.mean(0)) / x.std(0)
 y = data[:, 13:]
-
 xtrn = np.zeros((506, 1, 1, 13))
-ytrn = np.zeros((506, 1, 1, 1))
-
 xtrn[:,0,0,:] = x
-ytrn[:,0,0,:] = y
-
+ytrn = y.reshape((506, 1, 1, 1))
 caffe.set_mode_gpu()
 caffe.set_device(0)
+net = caffe.Net("housing.prototxt", caffe.TRAIN)
 solver = caffe.SGDSolver('housing_solver.prototxt')
-solver.net.set_input_arrays(xtrn.astype(np.float32), ytrn.astype(np.float32))
 gc.disable()
 t0 = timer()
-solver.solve()
+for i in range(10000):
+    solver.net.set_input_arrays(xtrn.astype(np.float32), ytrn.astype(np.float32))
+    solver.step(1)
 t1 = timer()
+
 gc.enable()
 print "Time: %.4f" % (t1 - t0)
