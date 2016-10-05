6a425da5d609d5d5242467b6175d529f5d718b2b
Ilker Kesen <ilkerksn@gmail.com>
2016-10-05 08:56:10 +0000

    commit leftovers

--- data/create_housing_lmdb.py
+++ data/create_housing_lmdb.py
@@ -5,38 +5,31 @@ import caffe
 import six.moves.cPickle as pickle
 
 
-def chunks(l, n):
-    return [l[i:i+n] for i in xrange(0, len(l), n)]
-
-
 def make_lmdb(data, savedir):
-    savedir = os.path.abspath(savedir) 
-    xdir = os.path.join(savedir, "xtrn")
-    ydir = os.path.join(savedir, "ytrn")
+    savedir = os.path.abspath(os.path.join(savedir))
     if not os.path.exists(savedir):
         os.makedirs(savedir)
 
-    xdb = lmdb.open(xdir, map_size=int(1e12), map_async=True, writemap=True)
-    ydb = lmdb.open(ydir, map_size=int(1e12), map_async=True, writemap=True)
-
+    xdb = lmdb.open(savedir, map_size=int(1e12), map_async=True, writemap=True)
     xtxn = xdb.begin(write=True)
-    ytxn = ydb.begin(write=True)
 
     data = data.transpose()
     n, m = 13, 506
     x = data[0:n]
     y = data[13,:]
     x = (x - x.mean(1).reshape(-1,1)) / x.std(1).reshape(-1,1)
+
+
+    datum = caffe.proto.caffe_pb2.Datum()
+    datum.channels = 1
+    datum.height = 1
+    datum.width = 13
     for i in range(m):
-        # import pdb;pdb.set_trace()
-        x = caffe.io.array_to_datum(data[0:n,i].reshape(1,1,13).astype(np.float))
-        y = caffe.io.array_to_datum(data[n,i].reshape(1,1,1).astype(np.float))
-        xtxn.put('{:0>10d}'.format(i), x.SerializeToString())
-        ytxn.put('{:0>10d}'.format(i), y.SerializeToString())
+        datum.data = x[:,i].reshape(13,1).tobytes()
+        datum.label = int(y[i])
+        xtxn.put('{:0>10d}'.format(i), datum.SerializeToString())
     xtxn.commit()
-    ytxn.commit()
     xdb.close()
-    ydb.close()
 
 
 def main():
