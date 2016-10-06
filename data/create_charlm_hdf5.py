1970d702ad1b0128b861eed84d6c02f3249cec51
Ilker Kesen <ilkerksn@gmail.com>
2016-10-06 00:23:08 +0000

    not sure if it true, but it converges

--- data/create_charlm_hdf5.py
+++ data/create_charlm_hdf5.py
@@ -8,26 +8,41 @@ import os
 SOURCE="/home/ec2-user/Knet8-Benchmarks/data/19.txt"
 with open(SOURCE, 'r') as f:
     txt = [c for c in f.read()] 
-   
+
+timesteps = 100
+batchsize = 128
 i, vocab = 0, dict()
 for c in set(txt):
     vocab[c] = i
     i += 1
 
-timesteps = 100
-batchsize = 128
-nsamples = len(txt) / timesteps
-source, target = [], []
+# clip last batch
+txt = txt[:-(len(txt) % (batchsize*timesteps))]
 
-for i in range(0, len(txt), timesteps)[:-1]:
-    source.append([vocab[txt[j]] for j in range(i, i+timesteps-1)])
-    target.append([vocab[txt[j]] for j in range(i+1, i+timesteps)])
-
-
-source = np.array(source).astype(np.uint8)
-target = np.array(target).astype(np.uint8)
+# source, target = [], []
+source = np.zeros((99*16,128), dtype=np.uint8)
+target = np.zeros((99*16,128), dtype=np.uint8)
 cont = np.ones(source.shape).astype(np.uint8)
-cont[:,0] = 0
+for i in range(cont.shape[0]):
+    if i % 99 == 0:
+        cont[i] = 0
+
+i, j, k = 0, 0, 0
+while True:
+    #print i, j, k
+    lo, up = i * timesteps, (i+1) * timesteps
+    so = np.array(map(lambda x: vocab[x], txt[lo:up-1])).astype(np.uint8)
+    ta = np.array(map(lambda x: vocab[x], txt[lo+1:up])).astype(np.uint8)
+    source[j:j+timesteps-1,k] = so.reshape(1,99)
+    target[j:j+timesteps-1,k] = ta.reshape(1,99)
+    k += 1
+    if k >= 128:
+        k = 0
+        j = j + timesteps - 1
+    i += 1
+    if j >= source.shape[0]:
+        break
+
 
 f = h5py.File("data.h5", "w")
 f.create_dataset('data', data=source)
@@ -36,4 +51,4 @@ f.create_dataset('cont', data=cont)
 f.close()
 
 print 'Done.'
-print 'vocab; %d, nbatches: %d' % (len(vocab), nsamples/batchsize)
+print 'vocab; %d' % (len(vocab))
