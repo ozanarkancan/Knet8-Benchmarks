7121be41776c5a0f3181481e79635a7ef858f325
Ilker Kesen <ilkerksn@gmail.com>
2016-10-06 02:20:49 +0000

    make everything automated

---
+++ caffe/runboth.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./rungpu.sh
+./runcpu.sh
---
+++ caffe/runcpu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+printf "CPU BULK RUN\n\n"
+
+printf "HOUSING\n"
+for i in `seq 1 5`; do
+	python housing.py 
+done
+
+printf "SOFTMAX\n"
+for i in `seq 1 5`; do
+	python runmnist.py --solver softmax_solver.prototxt 
+done
+
+printf "MLP\n"
+for i in `seq 1 5`; do
+	python runmnist.py --solver mlp_solver.prototxt 
+done
+
+printf "LENET\n"
+for i in `seq 1 5`; do
+	python runmnist.py --solver lenet_solver.prototxt 
+done
+
+printf "CHARLM\n"; do
+	python charlm.py --solver charlm_solver.prototxt 
+done 
